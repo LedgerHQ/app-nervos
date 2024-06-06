0f7c8967ccd6deeda9382bd2827ec5e5f48a48e0
Charles-Edouard de la Vergne <charles-edouard.delavergne@ledger.fr>
2024-06-06 15:57:13 +0200

    Port nbglv2

--- src/ui_nbgl.c
+++ src/ui_nbgl.c
@@ -11,6 +11,7 @@ enum {
     SIGN_HASH_TOKEN,
     CONTRACT_DATA_TOKEN
 };
+#define TOKEN_TO_ID(token) (token - TESTNET_ADDR_TOKEN)
 
 static const char* const infoTypes[] = {
     "Version"
@@ -20,107 +21,98 @@ static const char* const infoContents[] = {
     VERSION
 };
 
+#define SETTINGS_NB_SWITCHES 3
+static nbgl_layoutSwitch_t switches[SETTINGS_NB_SWITCHES] = {0};
 
-static nbgl_layoutSwitch_t switches[3];
-
-static bool settings_nav(uint8_t page, nbgl_pageContent_t *content) {
-    switch (page) {
-        case 0:
-            switches[0] = (nbgl_layoutSwitch_t) {
-                .initState = (N_data.address_type == ADDRESS_TESTNET) ? ON_STATE : OFF_STATE,
-                .text = "Testnet addresses",
-                .subText = "Instead of mainnet",
-                .token = TESTNET_ADDR_TOKEN,
-                .tuneId = TUNE_TAP_CASUAL
-            };
-            switches[1] = (nbgl_layoutSwitch_t) {
-                .initState = (N_data.sign_hash_type == SIGN_HASH_ON) ? ON_STATE : OFF_STATE,
-                .text = "Allow sign hash",
-                .subText = NULL,
-                .token = SIGN_HASH_TOKEN,
-                .tuneId = TUNE_TAP_CASUAL
-            };
-            switches[2] = (nbgl_layoutSwitch_t) {
-                .initState = (N_data.contract_data_type == ALLOW_CONTRACT_DATA) ? ON_STATE : OFF_STATE,
-                .text = "Allow contract data",
-                .subText = NULL,
-                .token = CONTRACT_DATA_TOKEN,
-                .tuneId = TUNE_TAP_CASUAL
-            };
-            content->type = SWITCHES_LIST;
-            content->switchesList.nbSwitches = 3;
-            content->switchesList.switches = (nbgl_layoutSwitch_t*)switches;
-            break;
-        case 1:
-            content->type = INFOS_LIST;
-            content->infosList.nbInfos = 1;
-            content->infosList.infoTypes = (const char**)infoTypes;
-            content->infosList.infoContents = (const char**)infoContents;
-            break;
-    }
-    return true;
-}
-
-static void settings_ctrl(int token, uint8_t index) {
-    (void)index;
+static void settings_ctrl(int token, uint8_t index, int page) {
+    UNUSED(index);
+    UNUSED(page);
     uint8_t value;
+    uint8_t switch_id;
 
+    switch_id = TOKEN_TO_ID(token);
     switch (token) {
         case TESTNET_ADDR_TOKEN:
             value = (N_data.address_type == ADDRESS_TESTNET) ? ADDRESS_MAINNET : ADDRESS_TESTNET;
+            switches[switch_id].initState = (nbgl_state_t) value;
             nvm_write((void*)&N_data.address_type, (void*)&value, sizeof(value));
             break;
         case SIGN_HASH_TOKEN:
             value = (N_data.sign_hash_type == SIGN_HASH_ON) ? SIGN_HASH_OFF : SIGN_HASH_ON;
+            switches[switch_id].initState = (nbgl_state_t) value;
             nvm_write((void*)&N_data.sign_hash_type, (void*)&value, sizeof(value));
             break;
         case CONTRACT_DATA_TOKEN:
             value = (N_data.contract_data_type == ALLOW_CONTRACT_DATA) ? DISALLOW_CONTRACT_DATA : ALLOW_CONTRACT_DATA;
+            switches[switch_id].initState = (nbgl_state_t) value;
             nvm_write((void*)&N_data.contract_data_type, (void*)&value, sizeof(value));
             break;
     }
 }
 
-static void ui_settings(void) {
-    nbgl_useCaseSettings(APPNAME" settings",
-                         0,
-                         2,
-                         true,
-                         ui_initial_screen,
-                         settings_nav,
-                         settings_ctrl);
-}
 
 void ui_initial_screen(void) {
-    nbgl_useCaseHome(APPNAME,
-                     &C_stax_nervos_64px,
-                     NULL,
-                     true,
-                     ui_settings,
-                     exit_app);
+    static nbgl_contentInfoList_t infosList = {0};
+    static nbgl_genericContents_t settingContents = {0};
+    static nbgl_content_t content = {0};
+
+    infosList.nbInfos = 1;
+    infosList.infoTypes = (const char**) infoTypes;
+    infosList.infoContents = (const char**) infoContents;
+
+    switches[0] = (nbgl_layoutSwitch_t) {
+        .initState = (N_data.address_type == ADDRESS_TESTNET) ? ON_STATE : OFF_STATE,
+        .text = "Testnet addresses",
+        .subText = "Instead of mainnet",
+        .token = TESTNET_ADDR_TOKEN,
+        .tuneId = TUNE_TAP_CASUAL
+    };
+    switches[1] = (nbgl_layoutSwitch_t) {
+        .initState = (N_data.sign_hash_type == SIGN_HASH_ON) ? ON_STATE : OFF_STATE,
+        .text = "Allow sign hash",
+        .subText = NULL,
+        .token = SIGN_HASH_TOKEN,
+        .tuneId = TUNE_TAP_CASUAL
+    };
+    switches[2] = (nbgl_layoutSwitch_t) {
+        .initState = (N_data.contract_data_type == ALLOW_CONTRACT_DATA) ? ON_STATE : OFF_STATE,
+        .text = "Allow contract data",
+        .subText = NULL,
+        .token = CONTRACT_DATA_TOKEN,
+        .tuneId = TUNE_TAP_CASUAL
+    };
+
+    content.type = SWITCHES_LIST;
+    content.content.switchesList.nbSwitches = SETTINGS_NB_SWITCHES;
+    content.content.switchesList.switches = switches;
+    content.contentActionCallback = settings_ctrl;
+
+    settingContents.callbackCallNeeded = false;
+    settingContents.contentsList = &content;
+    settingContents.nbContents = 1;
+
+    nbgl_useCaseHomeAndSettings(APPNAME,
+                                &C_stax_nervos_64px,
+                                NULL,
+                                INIT_HOME_PAGE,
+                                &settingContents,
+                                &infosList,
+                                NULL,
+                                exit_app);
 }
 
 static nbgl_layoutTagValue_t pair;
 static nbgl_layoutTagValueList_t pair_list;
-static nbgl_pageInfoLongPress_t info_long_press;
 char tag[MAX_SCREEN_COUNT][PROMPT_WIDTH + 1];
 char value[MAX_SCREEN_COUNT][VALUE_WIDTH + 1];
 
-static void onConfirmAbandon(void) {
-    nbgl_useCaseStatus("Transaction rejected", false, ui_initial_screen);
-    global.ui.cxl_callback();
-}
-
 static void reviewChoice(bool confirm) {
     if (confirm) {
-        nbgl_useCaseStatus("TRANSACTION\nSIGNED", true, ui_initial_screen);
         global.ui.ok_callback();
+        nbgl_useCaseReviewStatus(STATUS_TYPE_TRANSACTION_SIGNED, ui_initial_screen);
     } else {
-        nbgl_useCaseConfirm("Reject transaction?",
-                            "",
-                            "Yes, Reject",
-                            "Go back to transaction",
-                            onConfirmAbandon);
+        global.ui.cxl_callback();
+        nbgl_useCaseReviewStatus(STATUS_TYPE_TRANSACTION_REJECTED, ui_initial_screen);
     }
 }
 
@@ -146,15 +138,18 @@ void ui_prompt_with_cb(void (*switch_screen_cb)(size_t),
     pair_list.callback = get_pair;
     pair_list.startIndex = 0;
 
-    info_long_press.icon = &C_stax_nervos_64px;
-    info_long_press.text = "Confirm "APPNAME" action";
-    info_long_press.longPressText = "Hold to approve";
-
     global.ui.switch_screen = switch_screen_cb;
     global.ui.ok_callback = ok_c;
     global.ui.cxl_callback = cxl_c;
     global.ui.prompt.offset = MAX_SCREEN_COUNT - prompt_count;
-    nbgl_useCaseStaticReview(&pair_list, &info_long_press, "Reject", reviewChoice);
+
+    nbgl_useCaseReview(TYPE_TRANSACTION,
+                        &pair_list,
+                        &C_stax_nervos_64px,
+                        "Confirm "APPNAME" action",
+                        NULL,
+                        "Confirm "APPNAME" action",
+                        reviewChoice);
 }
 
 
